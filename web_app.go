6d957cc5e67e5d23c85c48592962570be8856c08
Syed Ahmed Jamil <ahmedjamilcs@gmail.com>
2023-05-30 16:51:40 +0500

    Create web_app.go

---
+++ web_app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello from lesson")
+  })
+  err := http.ListenAndServe("0.0.0.0:80", nil)
+  if err != nil {
+    panic(err)
+  }
+}
