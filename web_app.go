23ffbb036693a8ce4b22365ae98af49e72e6f814
Syed Ahmed Jamil <ahmedjamilcs@gmail.com>
2023-05-31 11:45:43 +0500

    Update web_app.go

--- web_app.go
+++ web_app.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello from lesson 5 UPDATED!")
+    fmt.Fprint(w, "Hello from lesson 5 UPDATED")
   })
   err := http.ListenAndServe("0.0.0.0:8080", nil)
   if err != nil {
